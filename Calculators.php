3fe190b65aa256f98c69d7685494b7702efb6029
J.Barker <43638273+Barker678@users.noreply.github.com>
2020-11-24 16:37:58 +0000

    what I've done so far
    since the split in the group and doing seperate projects i've donw work throughout the pages and started a custom page to hold the calculators and links for more depth will try to get this close to completeioon soon

---
+++ Calculators.php
@@ -0,0 +1,121 @@
+<?php
+/*
+ * Template Name: Custom Full Width
+ * description: >-
+  Page template without sidebar
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="content-area col-sm-12 col-lg-12">
+		<div id="main" class="site-main" role="main">
+			<div class="row">
+				<div class="col-12">
+					<h3>Calculators</h3>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates eum, numquam ipsa temporibus illum nam. Aut provident, itaque sint vitae quibusdam iste qui totam exercitationem porro laborum quia, modi quae?</p>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row mt-3">
+					<div class="col-4 ">
+						<div class="card" style="width: 18rem; background-color: #e0e0e0;">
+							<img src="http://jordenweb.uosweb.co.uk/wp-content/uploads/2020/11/S-1.jpg" class="card-img-top" style="height: 350px; object-fit: cover;" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">HP Prime</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a type="button" class="btn btn btn-lg btn-block">button 1</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-4 ">
+						<div class="card" style="width: 18rem; background-color: #e0e0e0;">
+							<img src="http://jordenweb.uosweb.co.uk/wp-content/uploads/2020/11/2c33f6978654b3c73a2e1e5d667e02cd.jpg" class="card-img-top" style="height: 350px; object-fit: cover;" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">HP-50</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a type="button" class="btn btn btn-lg btn-block">button 1</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-4 ">
+						<div class="card" style="width: 18rem; background-color: #e0e0e0;">
+							<img src="http://jordenweb.uosweb.co.uk/wp-content/uploads/2020/11/HP49G.jpg" class="card-img-top" style="height: 350px; object-fit: contain;" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">HP-49</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a type="button" class="btn btn btn-lg btn-block">button 1</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row mt-5">
+					<div class="col-4 ">
+						<div class="card" style="width: 18rem; background-color: #e0e0e0;">
+							<img src="http://jordenweb.uosweb.co.uk/wp-content/uploads/2020/11/hp48.jpg" class="card-img-top" style="height: 350px; object-fit: cover;" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">HP-48</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a type="button" class="btn btn btn-lg btn-block">button 1</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-4 ">
+						<div class="card" style="width: 18rem; background-color: #e0e0e0;">
+							<img src="http://jordenweb.uosweb.co.uk/wp-content/uploads/2020/11/42s3q.jpg" class="card-img-top" style="height: 350px; object-fit: cover;" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">HP-42</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a type="button" class="btn btn btn-lg btn-block">button 1</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-4 ">
+						<div class="card" style="width: 18rem; background-color: #e0e0e0;">
+							<img src="http://jordenweb.uosweb.co.uk/wp-content/uploads/2020/11/410013q.jpg" class="card-img-top" style="height: 350px; object-fit: cover;" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">HP-41</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a type="button" class="btn btn btn-lg btn-block">button 1</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row mt-5">
+					<div class="col-4 ">
+						<div class="card" style="width: 18rem; background-color: #e0e0e0;">
+							<img src="http://jordenweb.uosweb.co.uk/wp-content/uploads/2020/11/s-l400.jpg" class="card-img-top" style="height: 350px; object-fit: cover;" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">HP-39/40g</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a type="button" class="btn btn btn-lg btn-block">button 1</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-4 ">
+						<div class="card" style="width: 18rem; background-color: #e0e0e0;">
+							<img src="http://jordenweb.uosweb.co.uk/wp-content/uploads/2020/11/hp12.jpg" class="card-img-top" style="height: 350px; object-fit: cover;" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">HP-12c</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a type="button" class="btn btn btn-lg btn-block">button 1</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-4 ">
+						<div class="card" style="width: 18rem; background-color: #e0e0e0;">
+							<img src="http://jordenweb.uosweb.co.uk/wp-content/uploads/2020/11/P0.jpg" class="card-img-top" style="height: 350px; object-fit: cover;" alt="...">
+							<div class="card-body">
+								<h5 class="card-title">HP-10Bii</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								<a type="button" class="btn btn btn-lg btn-block">button 1</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();
